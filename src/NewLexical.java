f4a54b035207c76bdbffa28c770b11436bedd5f2
Leina Gray <abad.alleina@gmail.com>
2024-01-18 21:57:37 +0800

    DELETE: previous testing regex rules in useDelimiter()

--- src/NewLexical.java
+++ src/NewLexical.java
@@ -2,35 +2,28 @@ import java.util.Scanner;
 
 public class NewLexical {
     public static void main(String[] args) {
-        String code = "int num = 2%3 * 4 + \"hey\", \"hey world\" \"4+4\"";
+        String code = "int num = 2&&3.123,2 < 4 + \"hey\", \"hey world\" \"4+4\"";
         String code2 = "The A An Score could only be < 100\n"
-                        + "A Score could only be \"Very Good\", \"Good\", \"Pass\", \"Fail\" ";
+                        + "A Score could only be \"Very Good\", \"Good\", \"Pass\", \"4 + 4\" ";
 
         Scanner sc = new Scanner(code);
 
-        // ATTEMPT #3
-            //separate keywords by whitespaces
-            sc.useDelimiter("\s");
-
-            //separate literals from operators in expressions
-            String bef = "?<="; //look before
-            String aft = "?="; //look after
-            String digits = "\\d";
-            String spaces  = "\\s*";
-            String op = "[+-/*%<>=&|]"; 
-            String or = "|";
-
-            // String split_expressions = "("+bef + digits + spaces+")" + "("+aft + op+")"
-            //                          + or + "("+bef + op + spaces+")" + "("+aft + digits+")"
-            //                          + or + "("+bef + digits+")" + "("+aft + op+")"
-            //                          + or + "("+bef + op+")" + "("+aft + digits+")";
-            // String split_keywords = "\s";
-            // sc.useDelimiter(split_expressions);
-
-            //separate string literals only by double quotations
-            //"hello world"
-
-
+            //split code by...
+            sc.useDelimiter(//whitespace between keywords
+                            "\\s" 
+                            //if before is integer + whitespace, and after is operator
+                            + "|(?<=\\d\\s*)(?=[+-/*%<>=&|])" 
+                            //if before is operator + whitespace, and after is integer
+                            + "|(?<=[+-/*%<>=&|]\\s)(?=\\d)"
+                            //if before is integer, and after is operator
+                            + "|(?<=\\d)(?=[+-/*%<>=&|])"
+                            //if before is operator, and after is integer
+                            + "|(?<=[+-/*%<>=&|])(?=\\d)"
+                            //separate string literals with double quotation marks
+                            + "|\\s(?=\")(?=.)(?=\")"
+                            //separate literals with commas
+                            + "|(?=,\\s*)");
+        
         while (sc.hasNext()) {
             String token = sc.next();
             System.out.println(token);
